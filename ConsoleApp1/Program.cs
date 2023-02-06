48b90a56d588de09ea956585b662d142c547c459
F1ner <123871318+F1ner@users.noreply.github.com>
2023-02-06 11:17:15 +0200

    array

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Размер массива");
+            byte size = byte.Parse(Console.ReadLine());
+            int[] array = new int[size];
+            Random rnd = new Random();
+            for(int i=0;i<array.Length;i++)
+            {
+                array[i]=rnd.Next(-10,10);
+                Console.WriteLine($"array[{i}]={array[i]}");
+            }
+            int k = 0;
+            //    for(int i=0;i<array.Length;i++)
+            //{
+            //  if  (array[i]>0) k++;
+            //}
+            ///*Console.WriteLine($"k={k}");*/
+            foreach(int elem in array)
+            {
+                if (elem > 0) k++;
+            }
+            Console.WriteLine($"k={k}");
+            Console.ReadKey();
+        }
+    }
+}
